db4eacab96e5edbd49e7934d3ce4ded64e4dcec4
dhanushramudri <ramudridhanush@gmail.com>
2023-12-21 10:55:51 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+    serverComponentsExternalPackages: ["mongoose"],
+  },
+  images: {
+    // Specify remotePatterns as an object with domains or paths as keys
+    domains: ["public/assets", "m.media-amazon.com"], // Replace with your actual domain/path
+    formats: ["image/avif", "image/webp"], // Specify supported image formats for optimization
+    // Allow SVG
+    dangerouslyAllowSVG: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
